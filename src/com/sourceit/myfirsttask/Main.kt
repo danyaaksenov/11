a61583d7d1ce0194af7d6519f315094131dfa067
danaksenov18 <62466014+danaksenov18@users.noreply.github.com>
2020-05-22 23:06:42 +0300

    init11

--- src/com/sourceit/myfirsttask/Main.kt
+++ src/com/sourceit/myfirsttask/Main.kt
@@ -3,4 +3,7 @@ package com.sourceit.myfirsttask
 fun main(args: Array<String>) {
     println("Hello world!")
     println("Hello world!")
+    println("Hello esf!")
+
+    println("Hello world!")
 }
